fcfb48b033c034309dfd7ff42fbd73990f94331b
mteresas <mteresa.soares30@gmail.com>
2026-04-02 09:20:22 +0200

    Changes in Program.cs

--- IMDBImport/Program.cs
+++ IMDBImport/Program.cs
@@ -2,72 +2,108 @@
 using IMDBImport.Models;
 using Microsoft.Data.SqlClient;
 
-// what we are working with
 Console.WriteLine("IMDB Import");
 
-// list
-List<Title_Model> movies = new List<Title_Model>();
+List<Title_Model> titles = new List<Title_Model>();
 List<Genre_Model> genres = new List<Genre_Model>();
+List<Name_Model> names = new List<Name_Model>();
+List<NameProfession_Model> nameProfessions = new List<NameProfession_Model>();
+List<CrewDirector_Model> crewDirectors = new List<CrewDirector_Model>();
+List<CrewWriter_Model> crewWriters = new List<CrewWriter_Model>();
 
-// locate file from drive
-foreach (string movie in File.ReadLines("C:/temp/title.basics.tsv").Skip(1).Take(10))
+foreach (string line in File.ReadLines("C:/temp/title.basics.tsv").Skip(1))
 {
-	string[] movieParts = movie.Split('\t');
-	string[] genreParts = new string[2];
-	if (movieParts.Length == 9)
-	{
-		movies.Add(new Title_Model(movieParts));
+    string[] parts = line.Split('\t');
+    if (parts.Length == 9)
+    {
+        titles.Add(new Title_Model(parts));
 
-		string genreString = movieParts[8]; // reads the 8th part/index
-		try
-		{
-			string[] genreArray = genreString.Split(','); // string splits into an array of genres
-			foreach (string genre in genreArray)
-			{
-				genreParts[0] = movieParts[0]; // TConst
-				genreParts[1] = genre; // Genre
-				genres.Add(new Genre_Model(genreParts));
-			}
-		}
-		catch (Exception)
-		{
-			genreParts[0] = movieParts[0]; // TConst
-			genreParts[1] = movieParts[8]; // Genre (if only one genre, no comma, so it will be added as is)
-			genres.Add(new Genre_Model(genreParts));
-		}
-	}
-	else
-	{
-		Console.WriteLine("Invalid line: " + movie);
-	}
+        string genreString = parts[8];
+        try
+        {
+            string[] genreArray = genreString.Split(',');
+            foreach (string genre in genreArray)
+            {
+                genres.Add(new Genre_Model(new[] { parts[0], genre }));
+            }
+        }
+        catch (Exception)
+        {
+            genres.Add(new Genre_Model(new[] { parts[0], parts[8] }));
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid line: " + line);
+    }
 }
 
-/*
-// print movies
-foreach (var movie in movies)
+foreach (string line in File.ReadLines("C:/temp/name.basics.tsv").Skip(1))
 {
-	Console.WriteLine(movie.ToString());
-}
-*/
+    string[] parts = line.Split('\t');
+    if (parts.Length >= 4)
+    {
+        names.Add(new Name_Model(parts));
 
-// insert actual data
-//IInserter normalInserter = new NormalInserter();
-IInserter preparedInserter = new PreparedInserter();
-//IInserter bulkInserter = new BulkInserter();
+        if (parts.Length > 4 && parts[4] != "\\N")
+        {
+            string[] professionArray = parts[4].Split(',');
+            foreach (string profession in professionArray)
+            {
+                nameProfessions.Add(new NameProfession_Model(new[] { parts[0], profession }));
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid line: " + line);
+    }
+}
+
+foreach (string line in File.ReadLines("C:/temp/title.crew.tsv").Skip(1))
+{
+    string[] parts = line.Split('\t');
+    if (parts.Length >= 3)
+    {
+        string directorString = parts[1];
+        if (directorString != "\\N")
+        {
+            string[] directorArray = directorString.Split(',');
+            foreach (string director in directorArray)
+            {
+                crewDirectors.Add(new CrewDirector_Model(new[] { parts[0], director }));
+            }
+        }
+
+        string writerString = parts[2];
+        if (writerString != "\\N")
+        {
+            string[] writerArray = writerString.Split(',');
+            foreach (string writer in writerArray)
+            {
+                crewWriters.Add(new CrewWriter_Model(new[] { parts[0], writer }));
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid line: " + line);
+    }
+}
+
+IInserter bulkInserter = new BulkInserter();
 
 SqlConnection sqlConn = new SqlConnection(
-	"Server=localhost;Database=IMDB;Trusted_Connection=True;" +
-	"Trusted_Connection=True;TrustServerCertificate=True;");
-
-//sqlConn.Open();
-//normalInserter.InsertTitles(movies, sqlConn);
-//sqlConn.Close();
+    "Server=localhost;Database=IMDB;Trusted_Connection=True;" +
+    "Trusted_Connection=True;TrustServerCertificate=True;");
 
 sqlConn.Open();
-//preparedInserter.InsertTitles(movies, sqlConn);
-preparedInserter.InsertGenres(genres, sqlConn);
-sqlConn.Close();
 
-//sqlConn.Open();
-//bulkInserter.InsertTitles(movies, sqlConn);
-//sqlConn.Close();
+bulkInserter.InsertTitles(titles, sqlConn);
+bulkInserter.InsertGenres(genres, sqlConn);
+bulkInserter.InsertNames(names, sqlConn);
+bulkInserter.InsertNameProfessions(nameProfessions, sqlConn);
+bulkInserter.InsertCrewDirectors(crewDirectors, sqlConn);
+bulkInserter.InsertCrewWriters(crewWriters, sqlConn);
+
+sqlConn.Close();
